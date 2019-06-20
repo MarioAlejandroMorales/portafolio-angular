bc4b1d9d9bd8c71b3a0b148dd1d2cc823e540220
Mario Morales <mamg3108@gmail.com>
2019-06-20 18:27:27 -0500

    Fin seccion 5

--- src/app/services/info-paginas.service.ts
+++ src/app/services/info-paginas.service.ts
@@ -8,8 +8,13 @@ export class InfoPaginasService {
 
   info: any = {};
   cargada = false;
-
+  equipo: any[] = [];
   constructor(private http: HttpClient) {
+    this.cargarInfo();
+    this.cargarEquipo();
+  }
+
+  private cargarInfo() {
     console.log('Servicio infoPaginaService');
 
     this.http.get('assets/data/data-pagina.json')
@@ -17,8 +22,16 @@ export class InfoPaginasService {
         // console.log(resp['twitter']);
         this.cargada = true;
         this.info = resp;
-// tslint:disable-next-line: no-string-literal
-        console.log(this.info['nombre_corto'] );
+        // console.log(this.info['nombre_corto'] );
       });
   }
+
+  private cargarEquipo() {
+    this.http.get('https://angular-html-b3716.firebaseio.com/equipo.json')
+      .subscribe((resp: any[]) => {
+        this.equipo = resp;
+        console.log(this.equipo);
+      });
+  }
+
 }
