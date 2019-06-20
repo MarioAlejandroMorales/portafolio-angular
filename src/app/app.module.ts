558a3e1c7a10b87487237f911ba3d16a8e4d982c
Mario Morales <mamg3108@gmail.com>
2019-06-20 14:29:02 -0500

    Template listo sin rutas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PortafolioComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
