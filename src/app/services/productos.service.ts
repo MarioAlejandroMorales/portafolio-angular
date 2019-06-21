0c24d072f7bfa7b69e978d90f2cac8fd35657e84
Mario Morales <mamg3108@gmail.com>
2019-06-21 10:26:22 -0500

    Fin de todo

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,20 +9,54 @@ import { ProductoDescripcion } from '../interface/producto-descripcion.interface
 export class ProductosService {
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrados: Producto[] = [];
   constructor(private http: HttpClient) {
     this.cargarProductos();
   }
 
   private cargarProductos() {
-    this.http.get('https://angular-html-b3716.firebaseio.com/productos_idx.json')
-    .subscribe((resp: Producto[]) => {
-      //console.log(resp);
-      this.productos = resp;
-      this.cargando = false;
+
+    return new Promise((resolve, reject) => {
+      this.http.get('https://angular-html-b3716.firebaseio.com/productos_idx.json')
+      .subscribe((resp: Producto[]) => {
+        // console.log(resp);
+        this.productos = resp;
+        this.cargando = false;
+        resolve();
+      });
     });
+
   }
 
   getProducto(id: string) {
     return this.http.get(`https://angular-html-b3716.firebaseio.com/productos/${id}.json`);
+  }
+
+  buscarProducto(termino: string) {
+    if (this.productos.length === 0) {
+      this.cargarProductos().then( () => {
+        this.filtrarProductos(termino);
+      });
+    } else {
+      this.filtrarProductos(termino);
+    }
+  }
+
+  filtrarProductos(termino: string) {
+    /* this.productosFiltrados = this.productos.filter( producto =>{
+      return true;
+    });*/
+    // console.log(this.productosFiltrados);
+
+    this.productosFiltrados = [];
+    termino = termino.toLowerCase();
+
+    this.productos.forEach( prod => {
+      const titulo = prod.titulo.toLowerCase();
+      if (prod.categoria.indexOf(termino) >= 0 || titulo.indexOf(termino) >= 0) {
+        this.productosFiltrados.push(prod);
+      }
+    });
+
   }
 }
